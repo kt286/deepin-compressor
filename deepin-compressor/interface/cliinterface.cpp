508b965ed761a1f0dcf594bf12e3aabf004fe238
chendu <chendu@uniontech.com>
2020-06-15 14:26:50 +0800

    fix: 7z加密包追加压缩时不输入密码应用卡住，输入错误密码显示添加成功（实际未添加）
    Description: 追加压缩时命令添加密码参数
    
    Log: 追加压缩时命令添加密码参数
    
    link:

--- deepin-compressor/interface/cliinterface.cpp
+++ deepin-compressor/interface/cliinterface.cpp
@@ -365,6 +365,7 @@ bool CliInterface::runProcess(const QString &programName, const QStringList &arg
     m_process->setOutputChannelMode(KProcess::MergedChannels);
     m_process->setNextOpenMode(QIODevice::ReadWrite | QIODevice::Unbuffered | QIODevice::Text);
     m_process->setProgram(programPath, arguments);
+    // qDebug() << programPath << arguments;
 
     connect(m_process, &QProcess::readyReadStandardOutput, this, [ = ]() {
         readStdout();
